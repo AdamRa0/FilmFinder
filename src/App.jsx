e3d58e64dc30a5014d50f472bbfee6224c93f9cd
Adam Rao <adamokoth@protonmail.com>
2023-12-26 17:52:28 +0300

    chore: create box reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -88,18 +88,15 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function LeftBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && children }
+      {isOpen && children}
     </div>
   );
 }
@@ -126,29 +123,6 @@ function ListOfMoviesItem({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-function RightBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-        </>
-      )}
-    </div>
   );
 }
 
@@ -217,6 +191,7 @@ function WatchedSummary({ watched }) {
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -226,10 +201,13 @@ export default function App() {
         <NumOfResults movies={movies} />
       </NavBar>
       <Main>
-        <LeftBox>
+        <Box>
           <ListOfMovies movies={movies} />
-        </LeftBox>
-        <RightBox />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
+        </Box>
       </Main>
     </>
   );
